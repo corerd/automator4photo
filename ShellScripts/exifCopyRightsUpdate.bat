a924a6d67657df86d26d136927d008318804322d
corerd <corerd@live.com>
2017-08-29 05:26:23 +0200

    First commit

---
+++ ShellScripts/exifCopyRightsUpdate.bat
@@ -0,0 +1,124 @@
+@echo OFF
+rem ############################################################################
+rem NAME
+rem        exifCopyRightsUpdate - Write copyrights information in files
+rem
+rem SYNTAX
+rem        exifCopyRightsUpdate -A AUTHOR FILE...
+rem
+rem DESCRIPTION
+rem        Pass the arguments to the provided bash script
+rem        in order to add copyrights information.
+rem
+rem ARGUMENTS
+rem        -A AUTHOR    The name of the person who created the image
+rem        FILE...      One or more image file names
+rem
+rem REQUIRED
+rem        Windows Subsystem for Linux (WSL) to provide Bash.exe application.
+rem
+rem        exiftool by Phil Harvey
+rem        See: http://www.sno.phy.queensu.ca/~phil/exiftool/
+rem
+rem DIAGNOSTICS
+rem        This application exits with a status of 0 on success, or 1 if
+rem        an error occurred processing one or more image files.
+rem
+rem ----------------------------------------------------------------------------
+rem THE BEER-WARE LICENSE
+rem <corerd@live.com> wrote this file. As long as you retain this notice you
+rem can do whatever you want with this stuff. If we meet some day, and you think
+rem this stuff is worth it, you can buy me a beer in return.
+rem ----------------------------------------------------------------------------
+rem ############################################################################
+
+rem Move to the current directory
+cd %~dp0
+
+rem Parse parameters in batch files at DOS command line
+rem See: https://stackoverflow.com/a/14298769
+:arg_parse
+if "%~1"=="" goto end_arg_parse
+if "%~1"=="-A" goto set_Author
+rem else
+if not defined Photographer goto exit_syntax_error
+goto exifUpdate
+
+
+:set_Author
+rem Set Author argument
+shift
+if "%~1"=="" goto end_arg_parse
+set "Photographer=-Photographer=%~1"
+goto next_arg
+
+
+:exifUpdate
+rem Convert Windows path for Windows Ubuntu Bash
+rem Link: https://superuser.com/a/1113866
+set "nixFilePath=%~1"
+
+set "drive=%nixFilePath:~0,1%"
+
+set nixFilePath=%nixFilePath:~2%
+set "nixFilePath=%nixFilePath:\=/%"
+
+if %drive%==A set "drive=a"
+if %drive%==B set "drive=b"
+if %drive%==C set "drive=c"
+if %drive%==D set "drive=d"
+if %drive%==E set "drive=e"
+if %drive%==F set "drive=f"
+if %drive%==G set "drive=g"
+if %drive%==H set "drive=h"
+if %drive%==I set "drive=i"
+if %drive%==J set "drive=j"
+if %drive%==K set "drive=k"
+if %drive%==L set "drive=l"
+if %drive%==M set "drive=m"
+if %drive%==N set "drive=n"
+if %drive%==O set "drive=o"
+if %drive%==P set "drive=p"
+if %drive%==Q set "drive=q"
+if %drive%==R set "drive=r"
+if %drive%==S set "drive=s"
+if %drive%==T set "drive=t"
+if %drive%==U set "drive=u"
+if %drive%==V set "drive=v"
+if %drive%==W set "drive=w"
+if %drive%==X set "drive=x"
+if %drive%==Y set "drive=y"
+if %drive%==Z set "drive=z"
+
+set "nixFilePath=/mnt/%drive%%nixFilePath%"
+
+bash ./exifCopyRightsUpdate "%Photographer%" "%nixFilePath%"
+if errorlevel 1 goto exit_bash_error
+
+goto next_arg
+
+
+:next_arg
+shift
+goto arg_parse
+
+
+:end_arg_parse
+goto exit_good
+
+
+:exit_good
+pause
+exit /B 0
+
+
+:exit_syntax_error
+echo SYNTAX: exifCopyRightsUpdate -A AUTHOR FILE..
+pause
+exit /B 1
+
+
+:exit_bash_error
+echo !!! Bash script reports some errors !!!
+pause
+exit /B 1
